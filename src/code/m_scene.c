dced13fcc058b1fb8c08387907ab8fd827fedf87
emilybrooks <emilybrooksemilybrooks@gmail.com>
2023-07-24 10:49:55 -0700

    almost matching first function

--- src/code/m_scene.c
+++ src/code/m_scene.c
@@ -1,27 +1,112 @@
 #include "global.h"
 
-s32 func_800C59B0_jp(s32);                          /* extern */
-s32 func_800C5A08_jp(s32);                          /* extern */
+//   mSc_set_bank_status_after
+//   mSc_clear_bank_status
+//   Object_Exchange_keep_new_Player
+//   mSc_secure_exchange_keep_bank 
+//   mSc_background_dmacopy_controller 
+//   mSc_dmacopy_data_bank 
+//   mSc_bank_regist_check 
+//   mSc_regist_initial_exchange_bank 
+//   mSc_dmacopy_all_exchange_bank_sub
+//   mSc_dmacopy_all_exchange_bank 
+//   mSc_data_bank_ct 
+//   mSc_decide_exchange_bank 
+//   Scene_player_select 
+//   Scene_ct 
+//   Scene_Proc_Player_Ptr 
+//   Scene_Proc_Ctrl_Actor_Ptr 
+//   Scene_Proc_Actor_Ptr 
+//   Scene_Proc_Object_Exchange_Bank_Ptr 
+//   Scene_Proc_Door_Data_Ptr 
+//   Door_info_ct 
+//   Scene_Proc_Sound 
+//   set_item_info
+//   Scene_Proc_Field_ct 
+//   Scene_Proc_MyRoom_ct 
+//   Scene_Proc_ArrangeRoom_ct 
+//   Scene_Proc_ArrangeFurniture_ct 
+//   goto_other_scene 
+//   goto_next_scene 
+//   goto_emu_game 
+//   return_emu_game 
+
+#define OBJECT_BANK_COUNT 73
+
+typedef struct {
+    /* 0x000 */ s16 id;
+    /* 0x004 */ void* unk4;
+    /* 0x008 */ UNK_TYPE unk8;
+    /* 0x00C */ UNK_TYPE unkC;
+    /* 0x010 */ UNK_TYPE unk10;
+    /* 0x014 */ char pad[0x38];
+    /* 0x050 */ UNK_TYPE unk50;
+    /* 0x053 */ u8 unk53;
+} ObjectBank; // size = 0x54
+
+typedef struct {
+    /* 0x0000 */ ObjectBank bank[OBJECT_BANK_COUNT];
+    /* 0x17F4 */ char unk17F4[8];
+    /* 0x17FC */ s32 unk17FC;
+    /* 0x1800 */ s32 unk1800;
+    /* 0x1804 */ u32 unk1804;
+} ObjectExchange; // size = 
+
+typedef struct {
+    char unk0;
+} DoorInfo;
+
+// s32 func_800C59B0_jp(s32);                          /* extern */
+// s32 func_800C5A08_jp(s32);                          /* extern */
 
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_scene/func_800C59B0_jp.s")
+// #pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_scene/func_800C59B0_jp.s")
+s32 func_800C59B0_jp(ObjectExchange *objectExchange)
+{
+    ObjectBank *bank;
+    s32 ret;
+    s32 bankIndex;
+    s32 new_var;
+    int new_var2;
+    ret = -1;
+    new_var = OBJECT_BANK_COUNT;
+    new_var2 = objectExchange->unk17FC;
+    if (1)
+    {
+        bankIndex = new_var2;
+        bank = &objectExchange->bank[bankIndex];
+        for (; bankIndex < new_var;)
+        {
+            if (bank->id == 0)
+            {
+                ret = bankIndex;
+                break;
+            }
+            bankIndex++;
+            bank++;
+        }
+    }
+  return ret;
+}
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_scene/func_800C5A08_jp.s")
 
-// #pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_scene/func_800C5A60_jp.s")
-s32 func_800C5A60_jp(s32 arg0) {
-    s32 temp_v0;
-    s32 var_v1;
 
-    temp_v0 = func_800C59B0_jp(arg0);
-    var_v1 = temp_v0;
-    if (temp_v0 == -1) {
-        var_v1 = func_800C5A08_jp(arg0);
-    }
-    return var_v1;
-}
+#pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_scene/func_800C5A60_jp.s")
+//matching but i need to test other functions
+// s32 func_800C5A60_jp(s32 arg0) {
+//     s32 temp_v0;
+//     s32 var_v1;
 
+//     temp_v0 = func_800C59B0_jp(arg0);
+//     var_v1 = temp_v0;
+//     if (temp_v0 == -1) {
+//         var_v1 = func_800C5A08_jp(arg0);
+//     }
+//     return var_v1;
+// }
 
+// s32 func_800C5AA0_jp(ObjectBank* objectBank, ObjectExchange* objectExchange, s16 arg2);
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_scene/func_800C5AA0_jp.s")
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/code/m_scene/func_800C5B30_jp.s")
